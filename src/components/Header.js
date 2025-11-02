3695e7a7219487599ff21ad97bc0aebe931720ee
Naama00 <y0534178779@gmail.com>
2025-11-02 16:17:39 +0200

    change

--- src/components/Header.js
+++ src/components/Header.js
@@ -13,11 +13,6 @@ const tamar2="good luck!!!!!"
   return (
     <header className="py-4 bg-blue-600 text-white">
       <div className="container mx-auto flex justify-between items-center px-4">
-        <h1>our shop- your best choice</h1>
-        <h1 className="text-2xl font-bold">Shopping App</h1>
-        <h2 className="text-2xl font-bold">Hello to our Shopping</h2>
-        <h1 className="text-2xl font-bold">Naama & Tovi</h1>
-        <h1 className="text-2xl font-bold">Shopping App</h1>
         <h1 className >hello </h1>
         <h1>{tamarrotan}</h1>
         <nav className="flex space-x-6">
