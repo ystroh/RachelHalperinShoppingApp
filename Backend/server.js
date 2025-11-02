ba0fe01a335cd646fdeb0ded3aac56387041ff86
Naama00 <y0534178779@gmail.com>
2025-11-02 16:23:57 +0200

    sdsd

--- Backend/server.js
+++ Backend/server.js
@@ -15,7 +15,7 @@ app.use(express.json());
 app.use(cors());
 
 // Connect to MongoDB
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/shoppingapp', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:23232/shoppingapp', {
   useNewUrlParser: true,
   useUnifiedTopology: true
 })
